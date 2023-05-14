41aa7fc63b29f1a31776001bca426dd3ec5806eb
Sebastian Lenzlinger <74497638+sebaschi@users.noreply.github.com>
2023-05-14 15:21:52 +0200

    Setup C and Header Files for User Space Logger Detection.

---
+++ src/find_logger_proc.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // for printf()
+#include <stdlib.h> // for exit()
+#include <string.h> // for strcmp()
+#include <dirent.h> // for opendir(), readdir(), closedir()
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h> // for readlink(), chdir(), getcwd()
+#include <errno.h> // for errno                   
+
+#include "find_logger.h"
+
+
+
+
